e1914e085bdc92e586c3266c90c4a20d7f8f90e2
leosch92 <leonardo_schimpf@id.uff.br>
2018-04-12 17:02:44 -0300

    Add files via upload
    Impressão do comando BPLC e do funcionamento da tripla SMC para adição simples

--- smc.go
+++ smc.go
@@ -1,39 +1,51 @@
 package main
-import "fmt"
+import(
+	"fmt"
+	"strconv"
+)
 
 type SMC struct{
-	S stack
-	//Memória funciona como um mapa -> dado uma posição é encontrada uma info (não sei se é melhor solução)
+	S Stack
 	M map[string]string
-	C stack
+	C Stack
+}
+
+var evaluate = map[string] func(string, string) Tree{
+	"add": func (op1 string, op2 string) Tree{
+		e1,_ := strconv.Atoi(op1)
+		e2,_ := strconv.Atoi(op2)	
+		return Tree{Left:nil,Value:strconv.Itoa(e1+e2),Right:nil}
+	},
+}
+
+func iniciaSMC() SMC{
+	var smc = *new(SMC)
+	smc.M = make(map[string]string)
+	return smc
 }
 
 //Função En equivale à (S,M,nC)=>(nS,M,C), isto é o valor é transferido da pilha C para a pilha S
 func (smc SMC) En() SMC{
-	//Garante que existe dado na Pilha C
-	if smc.C.size()>0{
-		var dado = ""
-		smc.C, dado = smc.C.pop()
-		smc.S = smc.S.push(dado)
-	}
+	var dado = *(new(Tree))
+	smc.C,dado = smc.C.pop()
+	smc.S = smc.S.push(dado)
 	return smc
 }
 
 //Função Ev equivale à (S,M,vC)=>(M(v)S,M,C), isto é o valor na posição v em M é colocada na pilha S
-func (smc SMC) Ev() SMC{
-	if smc.C.size()>0{
-		var dado = ""
-		smc.C, dado = smc.C.pop()
-		smc.S = smc.S.push(smc.M[dado])
-	}
-	return smc
-}
+//func (smc SMC) Ev() SMC{
+//	var dado = *(new(Tree))
+//	smc.C, dado = smc.C.pop()
+//	smc.S = smc.S.push(smc.M[dado])
+//	return smc
+//}
 
+//Função Ee equivale à (S,M,e op e' C)=>(S,M,e e' op C)
 func (smc SMC) Ee()SMC{
 	if smc.C.size()>2{
-		var e = ""
-		var eLinha = ""
-		var operador = ""
+		var e = *(new(Tree))
+		var eLinha = *(new(Tree))
+		var operador = *(new(Tree))
 		smc.C,e = smc.C.pop()
 		smc.C,operador = smc.C.pop()
 		smc.C,eLinha = smc.C.pop()
@@ -45,14 +57,58 @@ func (smc SMC) Ee()SMC{
 }
 
 func (smc SMC) Ei()SMC{
-	if smc.C.size()>2{
-		var operando1 = ""
-		var operando2 = ""
-		var operacao = ""
-		smc.C,operando1 = smc.C.pop()
-		smc.C,operando2 = smc.C.pop()
-		smc.C,operacao = smc.C.pop()
-		smc.S = smc.S.push("eval("+operando1+operacao+operando2+")")
+	var operando1 = *(new(Tree))
+	var operando2 = *(new(Tree))
+	var operacao = *(new(Tree))
+	smc.S,operando2 = smc.S.pop()
+	smc.S,operando1 = smc.S.pop()
+	smc.C,operacao = smc.C.pop()
+	if(operando1.checkIfNode()&&operando2.checkIfNode()&&operacao.checkIfNode()){
+		smc.S = smc.S.push(
+			evaluate[operacao.toString()](operando1.toString(),operando2.toString()))
 	}
+	return smc
+}
+
+func (smc SMC) push_tree(tree Tree) (SMC) {
+	l_node, value, r_node := tree.dismember()
+	smc.C = smc.C.push(Tree{Left:nil, Value:value, Right:nil})
+	smc.C = smc.C.push(r_node)
+	smc.C = smc.C.push(l_node)
+	return smc
+}	
+
+func (tree Tree) dismember() (Tree, string, Tree) {
+	return *tree.Left, tree.Value, *tree.Right
+}
+
+func resolverSMC(smc SMC, t Tree)SMC{
+	
+	smc.C = smc.C.push(t)
+	fmt.Println(smc)
+		
+	for smc.C.size()>0{
+		
+		_,op := smc.C.pop()
+		if op.checkIfNode(){
+			_, isOperation := evaluate[op.toString()]
+			if isOperation {
+				//Se entrou aqui é porque topo da pilha é uma operação válida
+				smc = smc.Ei()
+			}
+			if !isOperation{
+				//Não consegui fazer else não sei porque, mas se entrou aqui é operando no topo da pilha
+				smc = smc.En()
+			}
+		}
+
+		if !op.checkIfNode(){
+			//Se entrou aqui topo da pilha é uma árvore com mais de um nó
+			smc.C,_ = smc.C.pop()
+			smc = smc.push_tree(op)
+		}
+		fmt.Println(smc)
+	}	
+
 	return smc
 }
--- stack.go
+++ stack.go
@@ -1,24 +1,24 @@
 package main
 
 //Implementação de Struct de Pilha-> utiliza slices para facilitar
-type stack struct{
-	 data []string
+type Stack struct{
+	 data []Tree
 }
 
-func (s stack) push(info string) stack{
+func (s Stack) push(info Tree) Stack{
 	s.data = append(s.data, info)
 	return s
 }
 
-func(s stack) pop() (stack,string){
+func(s Stack) pop() (Stack,Tree){
 	if len(s.data)==0{
-		return s, ""
+		return s, *new(Tree)
 	}
 	var info= s.data[len(s.data)-1]
 	s.data = s.data[:len(s.data)-1]
 	return s, info
 }
 
-func (s stack) size() int{
+func (s Stack) size() int{
 	return len(s.data)
 }
