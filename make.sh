ec1e65158e1cd52fbf70b3e13866c4d4b3f8bda3
jose-nascimento <josevolponilicio@gmail.com>
2018-04-25 11:44:19 -0300

    modificar script

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 echo "Iniciando processo..."
+echo "Brew sera instalado. Caso ja possui Hombrew instalado, pressione qualquer tecla, exceto enter, para abortar. Caso contrario, pressione enter para continuar"
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo "Brew instalado com sucesso."
@@ -6,8 +7,8 @@ echo "Brew instalado com sucesso."
 brew update
 echo "Brew foi atualizado"
 
+echo "Go sera instalado"
 brew install golang
-
 echo "Go instalado com sucesso"
 
 export PATH=/usr/local/go/bin:$PATH
@@ -21,9 +22,13 @@ export PATH=$GOBIN:$PATH
 echo $PATH
 echo "Env vars criadas"
 
+echo "Pigeon sera instalado"
 go get -u github.com/mna/pigeon
 echo "Pigeon instalado"
 
+rm -rf imperador_golias
+
+echo "Clonando projeto em diretoria local"
 git clone https://github.com/leosch92/imperador_golias.git
 echo "Projeto clonado"
 
