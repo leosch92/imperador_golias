2c5796ba10309776ec6a2ee5521ab1714bb52e3c
arthurFerraz9 <arthurmf9@gmail.com>
2018-07-04 12:27:03 -0300

    SMC permite criação e execução de procedimentos

--- src/EnviromentValue.go
+++ src/EnviromentValue.go
@@ -5,6 +5,7 @@ type Constante string
 
 type EnviromentValue interface{
 	converteParaString() string
+	WhatIsMyType() string
 }
 
 func (loc Location) converteParaString() string{
@@ -13,4 +14,20 @@ func (loc Location) converteParaString() string{
 
 func (constante Constante) converteParaString() string{
 	return string(constante)
+}
+
+func (tree *Tree) converteParaString() string{
+	return tree.toString()
+}
+
+func (loc Location) WhatIsMyType() string{
+	return "main.Location"
+} 
+
+func (constante Constante) WhatIsMyType() string{
+	return "main.Constante"
+}
+
+func (tree *Tree) WhatIsMyType() string{
+	return "main.*Tree"
 }
